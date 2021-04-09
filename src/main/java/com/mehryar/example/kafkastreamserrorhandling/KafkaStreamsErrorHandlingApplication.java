7dd6d33d689ae9166c32a9ce010e4f1947f025f8
Mehryar Maalem <mehryar.maalem@ibm.com>
2021-04-09 16:31:59 -0700

    initial commit

---
+++ src/main/java/com/mehryar/example/kafkastreamserrorhandling/KafkaStreamsErrorHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.mehryar.example.kafkastreamserrorhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsErrorHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamsErrorHandlingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mehryar/example/kafkastreamserrorhandling/KafkaStreamsErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehryar.example.kafkastreamserrorhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsErrorHandlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
